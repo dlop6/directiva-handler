958d9458e62ba6fdae4e64eeac797029918b6eb2
Diego <lop23747@uvg.edu.gt>
2025-04-06 19:03:08 -0600

    .env y cambios de cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use std::env;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // cargar  .env
+    dotenv().ok();
+
+    
+    let server_port = env::var("SERVER_PORT").unwrap_or_else(|_| "8080".to_string());
+
+    // crear y lanzar el servidor Actix
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(|| async { "Hola Mundo" }))
+    })
+    .bind(format!("0.0.0.0:{}", server_port))?
+    .run()
+    .await
 }
